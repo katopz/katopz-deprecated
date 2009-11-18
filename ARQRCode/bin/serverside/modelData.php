dae21002b2f9058604226ff2b749f7f4ec668494
katopz <katopz@1b6a1934-45cb-11de-a1df-459bf3fe8569>
2009-11-18 20:28:08 +0000

    add model for testing, temporary remove php for faster testing

--- ARQRCode/bin/serverside/modelData.php
+++ ARQRCode/bin/serverside/modelData.php
@@ -1,8 +1,27 @@
 <?php
-if($_GET['code']=="A2A916")
+error_reporting(0);
+
+// fake database
+// G2, G6, G7, G8
+// ข้าว, กุ้ง, ไข่, หอมใหญ่
+// 058454, 26B0EA, 2AFF7A, 2BD35D
+
+$code = $_GET['code'];
+ 
+if($code=="058454")
 {
-	echo "serverside/J7.dae";
-}else{
-	echo "serverside/G2.dae";
+	echo "name=ข้าว&src=serverside/G2.dae";
+}
+else if($code=="26B0EA")
+{
+	echo "name=กุ้ง&src=serverside/G6.dae";
+}
+else if($code=="2AFF7A")
+{
+	echo "name=ไข่&src=serverside/G7.dae";
+}
+else// if($code=="2BD35D")
+{
+	echo "name=หอมใหญ่&src=serverside/G8.dae";
 }
 ?>
